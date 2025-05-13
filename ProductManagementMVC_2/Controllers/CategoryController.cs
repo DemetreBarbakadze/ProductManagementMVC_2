64159f720f8ca0c547e947f0c624a04124f9f7c2
demetre <demetre@DESKTOP-V3MRKI6>
2025-05-14 00:29:09 +0400

    Add project files.

---
+++ ProductManagementMVC_2/Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProductManagementMVC_2.Interfaces;
+using ProductManagementMVC_2.Models;
+
+namespace ProductManagementMVC_2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public IActionResult GetCategories(string searchString)
+        {
+            var objCategoryList = _categoryService.GetCategories(searchString);
+            return View(objCategoryList);
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Create(CategoryModel category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _categoryService.CreateCategory(category);
+                return RedirectToAction("GetCategories");
+            }
+            return View(category);
+        }
+
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var categoryToDelete = _categoryService.GetCategory(id);
+            if (categoryToDelete == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryToDelete);
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCategory(int id)
+        {
+            var getCategory = _categoryService.GetCategory(id);
+            if (getCategory == null)
+            {
+                return NotFound();
+            }
+            _categoryService.DeleteCategory(id);
+            return RedirectToAction("GetCategories");
+        }
+
+        [Authorize]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            var categoryToUpdate = _categoryService.GetCategory(id);
+            if (categoryToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryToUpdate);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult UpdateCategory(CategoryModel updatedCategory)
+        {
+            var getCategory = _categoryService.GetCategory(updatedCategory.Id);
+            if (getCategory == null)
+            {
+                return NotFound();
+            }
+            _categoryService.UpdateCategory(updatedCategory);
+            return RedirectToAction("GetCategories");
+        }
+
+
+
+
+
+    }
+}
